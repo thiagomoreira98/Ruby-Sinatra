0633481d24d0e54e11b3212d6a484df109ae05c9
Thiago Moreira <moreira.g.thiago@gmail.com>
2017-04-17 22:58:12 -0300

    add projects folder's

---
+++ chapter03/main.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'slim'
+require 'sass'
+require './song'
+
+get('/styles.css'){scss :styles }
+
+get '/' do
+ 	slim :home
+end
+
+get '/about' do
+ 	@title = "All About This Website"
+ 	slim :about
+end
+
+get '/contact' do
+  	slim :contact
+end
+
+not_found do
+	slim :not_found
+end
